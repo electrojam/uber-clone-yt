4a938bbb4d5d7c2aece5cfe199f188647d95eba6
electrojam <electrojam@hotmail.com>
2022-08-28 09:07:51 -0500

    begining HomeScreen & NavOptions

---
+++ screens/HomeScreen.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={[tw`bg-white h-full`]}>
+      <View style={[tw`p-5`]}>
+        <Image
+          style={{
+            width: 80,
+            height: 80,
+            resizeMode: "contain",
+          }} 
+          source={{
+            uri: "https://links.papareact.com/gzs"
+          }}
+        />
+
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({})
